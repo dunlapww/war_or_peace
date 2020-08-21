282d42525e85e89453d647c030f33c3aab46bf47
Will Dunlap <dunlapww@gmail.com>
2020-08-21 12:58:57 -0600

    Add standard deck test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -40,7 +40,7 @@ class DeckTest < Minitest::Test
     assert_equal @card2, @deck.remove_card
     assert_equal @card3, @deck.remove_card
     assert_equal [], @deck.cards
-    assert_equal nil, @deck.remove_card
+    assert_nil @deck.remove_card
   end
 
 
@@ -56,6 +56,71 @@ class DeckTest < Minitest::Test
     assert_equal [@card2, @card3, card4], @deck.cards
     assert_equal [@card3], @deck.high_ranking_cards
     assert_equal 33.33, @deck.percent_high_ranking
+
+  end
+
+  def test_generate_standard_deck
+    test_deck = Deck.new()
+    test_deck.generate_standard_deck
+
+    deck_details = test_deck.cards.map do |card|
+      "#{card.value} of #{card.suit.to_s} (#{card.rank})"
+    end.sort
+
+    assert_equal [
+      "10 of clubs (10)",
+      "10 of diamonds (10)",
+      "10 of hearts (10)",
+      "10 of spaids (10)",
+      "2 of clubs (2)",
+      "2 of diamonds (2)",
+      "2 of hearts (2)",
+      "2 of spaids (2)",
+      "3 of clubs (3)",
+      "3 of diamonds (3)",
+      "3 of hearts (3)",
+      "3 of spaids (3)",
+      "4 of clubs (4)",
+      "4 of diamonds (4)",
+      "4 of hearts (4)",
+      "4 of spaids (4)",
+      "5 of clubs (5)",
+      "5 of diamonds (5)",
+      "5 of hearts (5)",
+      "5 of spaids (5)",
+      "6 of clubs (6)",
+      "6 of diamonds (6)",
+      "6 of hearts (6)",
+      "6 of spaids (6)",
+      "7 of clubs (7)",
+      "7 of diamonds (7)",
+      "7 of hearts (7)",
+      "7 of spaids (7)",
+      "8 of clubs (8)",
+      "8 of diamonds (8)",
+      "8 of hearts (8)",
+      "8 of spaids (8)",
+      "9 of clubs (9)",
+      "9 of diamonds (9)",
+      "9 of hearts (9)",
+      "9 of spaids (9)",
+      "Ace of clubs (14)",
+      "Ace of diamonds (14)",
+      "Ace of hearts (14)",
+      "Ace of spaids (14)",
+      "Jack of clubs (11)",
+      "Jack of diamonds (11)",
+      "Jack of hearts (11)",
+      "Jack of spaids (11)",
+      "King of clubs (13)",
+      "King of diamonds (13)",
+      "King of hearts (13)",
+      "King of spaids (13)",
+      "Queen of clubs (12)",
+      "Queen of diamonds (12)",
+      "Queen of hearts (12)",
+      "Queen of spaids (12)"
+     ], deck_details
 
   end
 end
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -5,25 +5,39 @@ require './lib/turn'
 require './lib/game'
 
 #variables to setup a standard deck of 52 cards
-card_values = %w(2, 3, 4, 5, 6, 7, 8, 9, 10, Jack, Queen, King, Ace)
-card_ranks  = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-card_hash   = card_values.zip(card_ranks).to_h
-suits       = %i(hearts, clubs, diamonds, spaids)
-full_deck   = []
+card_suits        = [:hearts, :clubs, :diamonds, :spaids]
+card_values_ranks = {
+  "2,"    =>2,
+  "3,"    =>3,
+  "4,"    =>4,
+  "5,"    =>5,
+  "6,"    =>6,
+  "7,"    =>7,
+  "8,"    =>8,
+  "9,"    =>9,
+  "10,"   =>10,
+  "Jack," =>11,
+  "Queen,"=>12,
+  "King," =>13,
+  "Ace"   =>14
+}
 
 #create standard deck of 52 cards
-suits.each do |suit|
-  card_hash.each do |value,rank|
-    full_deck << Card.new(suit,value,rank)
-  end
-end
+standard_deck = Deck.new()
+standard_deck = standard_deck.generate_standard_deck
+# standard_deck = []
+# card_suits.each do |suit|
+#   card_values_ranks.each do |value,rank|
+#     full_deck << Card.new(suit,value,rank)
+#   end
+# end
 
 #shuffle the cards
-full_deck.shuffle!
+standard_deck.shuffle!
 
 #divide the 52 card deck into equal decks
-deck1 = Deck.new(full_deck.shift(26))
-deck2 = Deck.new(full_deck)
+deck1 = Deck.new(standard_deck.shift(26))
+deck2 = Deck.new(standard_deck)
 
 #setup two players, each with a deck
 player1 = Player.new("Alex",deck1)
